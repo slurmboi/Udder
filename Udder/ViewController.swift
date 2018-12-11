b45967cf978ea678c5ee5cafe2b28c301fed565d
Jaime Carrasco <jaimecarrasco@jaimes-macbook-pro.local>
2018-12-10 22:40:12 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jaime Carrasco on 12/10/18.
+//  Copyright © 2018 Jaime Carrasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
